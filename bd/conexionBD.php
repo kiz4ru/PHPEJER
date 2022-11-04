74e65268440404a581b0cf5cd3b55934c3c5886e
inigo <ladrongue5@gmail.com>
2022-11-04 12:06:56 +0100

    conexionbd

---
+++ bd/conexionBD.php
@@ -0,0 +1,16 @@
+<?php
+$servidor = "localhost";
+$usuario = "prueba";
+$contra = "prueba123";
+$bd = "conectar";
+
+$conexion = @mysql_connect($servidor, $usuario, $contrasenha); 
+
+if (!$conexion) {
+    die('<strong>No pudo conectarse:</strong> ' . mysql_error());
+}else {
+    echo "Conectado ala base de datos";
+}
+
+mysql_select_db($BD, $conexion) or die(mysql_error($conexion)); 
+?>
